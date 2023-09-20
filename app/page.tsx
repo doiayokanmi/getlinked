c81ea722d6e0d35e4d7bfd507e474da1dffc5ed2
Ibrahim Damilola Dhikirullahi <oloyedammy@gmail.com>
2023-09-20 16:02:27 +0100

    intro done

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,14 @@ export default function Home() {
     {
       heading: 'Introduction to getlinked',
       sub_heading: 'tech Hackathon 1.0',
-      image: "/images/The Big Idea.png",
+      image: "/images/The big idea.png",
       reversed: false,
       paragraph: "Our tech hackathon is a melting pot of visionaries, and its purpose is as clear as day: to shape the future. Whether you're a coding genius, a design maverick, or a concept wizard, you'll have the chance to transform your ideas into reality. Solving real-world problems, pushing the boundaries of technology, and creating solutions that can change the world, that's what we're all about!",
     },
     {
       heading: 'Introduction to getlinked',
       sub_heading: 'tech Hackathon 1.0',
-      image: "/images/The Big Idea.png",
+      image: "/images/The big idea.png",
       reversed: true,
       paragraph: "Our tech hackathon is a melting pot of visionaries, and its purpose is as clear as day: to shape the future. Whether you're a coding genius, a design maverick, or a concept wizard, you'll have the chance to transform your ideas into reality. Solving real-world problems, pushing the boundaries of technology, and creating solutions that can change the world, that's what we're all about!",
     },
